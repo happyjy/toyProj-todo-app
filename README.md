9505c0007075899a6f70b8a1f7302bd90c65cb16
happyjy <okwoyjy@gmail.com>
2020-05-31 11:50:58 +0900

    [기능추가]List item render 하기

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 - & + & selector
 - text-decoration: line-through;
+- classnames
+  - TodoListItem.js
+  - [classname npm](https://www.npmjs.com/package/classnames)
--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기1',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '리액트의 기초 알아보기2',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '리액트의 기초 알아보기3',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
