bc1010b3e04e4853d4050ba06e9282abcf188e5b
happyjy <okwoyjy@gmail.com>
2020-05-31 15:11:41 +0900

    [성능개선]useReducer사용으로 onToggle, onRemove가 새로 생기는 현상 방지

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useReducer } from 'react';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기1',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '리액트의 기초 알아보기2',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '리액트의 기초 알아보기3',
+function createBulkTodos() {
+  const arr = [];
+  for (let i = 1; i <= 2500; i++) {
+    arr.push({
+      id: i,
+      text: `할 일 ${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return arr;
+}
 
-  const nextId = useRef(4);
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT': //새로추가
+      return todos.concat(action.todo);
+    case 'REMOVE': //제거
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE': //토글
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
+function App() {
+  // 두번째 파라미터에 초기 상태를 넣어주어야함
+  // 지금 처럼 두번째 파라미터에 undefined, 세번째 파라미터에 createBulkTodos 넣어주면 컴포넌트가 맨 처음 렌더링될 때만 creatBulkTodos 함수가 호출
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+
+  const nextId = useRef(2501);
+
+  //useReducer 사용시
   const onInsert = useCallback(
     (text) => {
-      debugger;
       const todo = {
         id: nextId.current,
         text,
         checked: false,
       };
-      setTodos(todos.concat(todo));
+      dispatch({ type: 'INSERT', todo });
       nextId.current += 1;
     },
     [todos],
@@ -40,22 +54,20 @@ function App() {
 
   const onRemove = useCallback(
     (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
+      dispatch({ type: 'REMOVE', id });
     },
     [todos],
   );
 
   const onToggle = useCallback(
     (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
+      debugger;
+      dispatch({ type: 'TOGGLE', id });
     },
     [todos],
   );
 
+  // useState사용시
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
